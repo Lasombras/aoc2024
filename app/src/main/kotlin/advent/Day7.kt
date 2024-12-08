a08899ad7131443d4a0754ac7586463f98b6b595
Eglem, Yannick (DI MO) - AF <yaeglem@airfrance.fr>
2024-12-08 09:00:32 +0100

    Clean up

--- app/src/main/kotlin/advent/Day7.kt
+++ app/src/main/kotlin/advent/Day7.kt
@@ -5,13 +5,13 @@ import tools.ResourceReader
 fun main() {
     val input = ResourceReader("day7.txt").useLines { seq ->
         seq.map { line ->
-            line.split(" ")
+            line.split(" ", ": ").map { it.toLong() }
         }.toList()
     }.orEmpty()
 
     input.sumOf { line ->
-        val target = line.first().dropLast(1).toLong()
-        val values = line.drop(1).map { it.toLong() }
+        val target = line.first()
+        val values = line.drop(1)
         if (calculate(1, values, target, Operator.MULTIPLY)) target else 0
     }.let {
         //Day 7 - Part 1 result
@@ -20,20 +20,18 @@ fun main() {
 }
 
 private enum class Operator {
-    NONE, SUM, MULTIPLY, CONCAT
+    SUM, MULTIPLY, CONCAT
 }
 
 private fun calculate(value: Long, values: List<Long>, target: Long, operator: Operator): Boolean {
-
     val total = when (operator) {
         Operator.SUM -> value + values.first()
         Operator.MULTIPLY -> value * values.first()
         Operator.CONCAT -> (value.toString() + values.first().toString()).toLong()
-        Operator.NONE -> value
     }
     if(total > target)
         return false
-    val nextValues = if (operator == Operator.NONE) values else values.drop(1)
+    val nextValues = values.drop(1)
     if (nextValues.isEmpty())
         return total == target
     return calculate(total, nextValues, target, Operator.SUM) ||
